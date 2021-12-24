70b218ee88564f4002db1f6d14132aff3d115f2f
Joy bhattacharya <joy.arya7381@gmail.com>
2021-12-24 10:46:15 +0530

    10:45 am 24 dec feature 1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -71,6 +71,7 @@ const userCreation = async(req, res) => {
         if (!validator.isValid(address)) {
             return res.status(400).send({ status: false, message: "Address is required" })
         }
+        //shipping address validation
         if (address.shipping.street) {
             if (!validator.isValidRequestBody(address.shipping.street)) {
                 return res.status(400).send({
@@ -81,6 +82,7 @@ const userCreation = async(req, res) => {
         } else {
             return res.status(400).send({ status: false, message: " Invalid request parameters. Shipping address's street cannot be empty" })
         }
+
         if (address.shipping.city) {
             if (!validator.isValidRequestBody(address.shipping.city)) {
                 return res.status(400).send({
@@ -192,7 +194,7 @@ const userLogin = async function(req, res) {
         const token = await jwt.sign({
             userId: userId,
             iat: Math.floor(Date.now() / 1000), //time of issuing the token.
-            exp: Math.floor(Date.now() / 1000) + 60 * 24 * 7 //setting token expiry time limit.
+            exp: Math.floor(Date.now() / 1000) + 3600 * 24 * 7 //setting token expiry time limit.
         }, 'secretKey')
 
         return res.status(200).send({
@@ -211,29 +213,29 @@ const userLogin = async function(req, res) {
 const getProfile = async(req, res) => {
     try {
         const userId = req.params.userId
-
-        //validation starts
+        const userIdFromToken = req.userId
+            //validation starts
         if (!validator.isValidObjectId(userId)) {
             return res.status(400).send({ status: false, message: "Invalid userId in params." })
         }
         //validation ends
 
-        const findUser = await userModel.findOne({ _id: userId })
-        if (!findUser) {
+        const findUserProfile = await userModel.findOne({ _id: userId })
+        if (!findUserProfile) {
             return res.status(400).send({
                 status: false,
                 message: `User doesn't exists by ${userId}`
             })
         }
         //Checking the authorization of the user -> Whether user's Id matches with the book creater's Id or not.
-        if (userId != findUser._id) {
+        if (userIdFromToken != findUserProfile._id) {
             return res.status(403).send({
                 status: false,
                 message: "Unauthorized access."
             })
         }
 
-        return res.status(200).send({ status: true, message: "Profile found successfully.", data: findUser })
+        return res.status(200).send({ status: true, message: "Profile found successfully.", data: findUserProfile })
     } catch (err) {
         return res.status(500).send({
             status: false,
@@ -247,76 +249,152 @@ const updateProfile = async(req, res) => {
         let files = req.files;
         const userId = req.params.userId
         const requestUpdateBody = req.body
-        const { fname, lname, email, phone, password, address, profileImage } = requestUpdateBody
+        const userIdFromToken = req.userId
 
+        const { fname, lname, email, phone, password, address } = requestUpdateBody
         //validation starts
         if (!validator.isValidObjectId(userId)) {
             return res.status(400).send({ status: false, message: "Invalid userId in params." })
         }
-        if (!validator.isValidRequestBody(requestUpdateBody)) {
-            return res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide  user details to update.' })
-        }
-        //validation ends
 
-        if (fname || lname || email || phone || password || address || profileImage) {
-            //validation for empty strings/values.
-            if (!validator.validString(fname)) {
-                return res.status(400).send({ status: false, message: "fname is missing ! Please provide the fname details to update." })
+        if (validator.isValidRequestBody(files)) {
+            if (!(files && files.length > 0)) {
+                return res.status(400).send({ status: false, message: "Invalid request parameter, please provide profile image" })
             }
-            if (!validator.validString(lname)) {
-                return res.status(400).send({ status: false, message: "lname is missing ! Please provide the lname details to update." })
+            var updateProfileImage = await config.uploadFile(files[0]);
+        }
+        if (requestUpdateBody) {
+            if (!validator.isValidRequestBody(requestUpdateBody)) {
+                return res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide user details to update.' })
             }
-            if (!validator.validString(email)) {
-                return res.status(400).send({ status: false, message: "email is missing ! Please provide the email details to update." })
+            //validation ends
+
+            if (fname || lname || email || phone || password || address) {
+                //validation for empty strings/values.
+                if (!validator.validString(fname)) {
+                    return res.status(400).send({ status: false, message: "fname is missing ! Please provide the fname details to update." })
+                }
+                if (!validator.validString(lname)) {
+                    return res.status(400).send({ status: false, message: "lname is missing ! Please provide the lname details to update." })
+                }
+                if (!validator.validString(email)) {
+                    return res.status(400).send({ status: false, message: "email is missing ! Please provide the email details to update." })
+                }
+                if (!validator.validString(phone)) {
+                    return res.status(400).send({ status: false, message: "phone number is missing ! Please provide the phone number to update." })
+                }
+                if (!validator.validString(password)) {
+                    return res.status(400).send({ status: false, message: "password is missing ! Please provide the password to update." })
+                }
+                //  console.log(address)
+
+                // if (!validator.isValidRequestBody(address)) {
+                //     return res.status(400).send({ status: false, message: "Address is missing ! Please provide the Address to update." })
+                // }
+                //shipping address validation
+                if (address.shipping.street) {
+                    if (!validator.validString(address.shipping.street)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Shipping address's Street Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: " Invalid request parameters. Shipping address's street cannot be empty" })
+                // }
+
+                if (address.shipping.city) {
+                    if (!validator.validString(address.shipping.city)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Shipping address city Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: "Invalid request parameters. Shipping address's city cannot be empty" })
+                // }
+                if (address.shipping.pincode) {
+                    if (!validator.validString(address.shipping.pincode)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Shipping address's pincode Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: "Invalid request parameters. Shipping address's pincode cannot be empty" })
+                // }
+                // Billing Adress validation
+                if (address.billing.street) {
+                    if (!validator.validString(address.billing.street)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Billing address's Street Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: " Invalid request parameters. Billing address's street cannot be empty" })
+                // }
+                if (address.billing.city) {
+                    if (!validator.validString(address.billing.city)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Billing address's city Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: "Invalid request parameters. Billing address's city cannot be empty" })
+                // }
+                if (address.billing.pincode) {
+                    if (!validator.validString(address.billing.pincode)) {
+                        return res.status(400).send({
+                            status: false,
+                            message: "Billing address's pincode Required"
+                        })
+                    }
+                } //else {
+                //     return res.status(400).send({ status: false, message: "Invalid request parameters. Billing address's pincode cannot be empty" })
+                // }
             }
-            if (!validator.validString(phone)) {
-                return res.status(400).send({ status: false, message: "phone number is missing ! Please provide the phone number to update." })
+            const findUserProfile = await userModel.findOne({ _id: userId })
+            if (!findUserProfile) {
+                return res.status(400).send({
+                    status: false,
+                    message: `User profile doesn't exists by ${userId}`
+                })
             }
-            if (!validator.validString(password)) {
-                return res.status(400).send({ status: false, message: "password is missing ! Please provide the password to update." })
+
+            //Authorizing user -> only the owner of the profile can update the details.
+            if (findUserProfile._id != userIdFromToken) {
+                return res.status(403).send({
+                    status: false,
+                    message: "Unauthorized access."
+                })
             }
-            if (!validator.validString(profileImage)) {
-                return res.status(400).send({ status: false, message: "profileImage is missing ! Please provide the profileImage to update." })
+
+            //finding email and phone in DB to maintain their uniqueness.
+            const findEmail = await userModel.findOne({ email: email })
+            if (findEmail) {
+                return res.status(400).send({
+                    status: false,
+                    message: `${email} already exists. Cannot update!`
+                })
+            }
+            const findPhone = await userModel.findOne({ phone: phone })
+            if (findPhone) {
+                return res.status(400).send({
+                    status: false,
+                    message: `${phone} already exists. Cannot update!`
+                })
+            }
+
+            if (requestUpdateBody.password) {
+                var encryptedPassword = await bcrypt.hash(requestUpdateBody.password, saltRounds)
             }
         }
-        const findProfile = await userModel.findOne({ _id: userId })
-        if (!findProfile) {
-            return res.status(400).send({
-                status: false,
-                message: `User profile doesn't exists by ${userId}`
-            })
-        }
-
-        //Authorizing user -> only the owner of the profile can update the details.
-        // if (findProfile._id != userId) {
-        //     return res.status(403).send({
-        //         status: false,
-        //         message: "Unauthorized access."
-        //     })
-        // }
-
-        //finding email and phone in DB to maintain their uniqueness.
-        const findEmail = await userModel.findOne({ email: email })
-        if (findEmail) {
-            return res.status(400).send({
-                status: false,
-                message: `${email} already exists. Cannot update!`
-            })
-        }
-        const findPhone = await userModel.findOne({ phone: phone })
-        if (findPhone) {
-            return res.status(400).send({
-                status: false,
-                message: `${phone} already exists. Cannot update!`
-            })
-        }
-
-        const updateProfileImage = await config.uploadFile(files[0]);
-        const encryptedPassword = await bcrypt.hash(requestUpdateBody.password, saltRounds)
-
         const changeProfileDetails = await userModel.findOneAndUpdate({ _id: userId }, { fname: fname, lname: lname, email: email, phone: phone, password: encryptedPassword, profileImage: updateProfileImage, address: address }, { new: true })
 
         return res.status(200).send({ status: true, message: "Successfully updated profile details.", data: changeProfileDetails })
+
     } catch (err) {
         return res.status(500).send({
             status: false,
